f9df436553eb8076eb7746ae7bc7d70929db96a0
AnaelBonnafous <71386173+AnaelBonnafous@users.noreply.github.com>
2022-12-01 23:47:10 +0100

    fetch and list products + handle error and no data

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
 import { ref, watch } from 'vue'
 
 const searchTerm = ref('')
+const products = ref([])
 
-const findProducts = async term => {}
+const findProducts = async term => {
+  try {
+    const { products: data } = await (await fetch(`https://dummyjson.com/products/search?q=${term}&limit=5`)).json()
+    products.value = data
+  } catch (error) {
+    alert('An error occurred while searching products. Please try again later.')
+  }
+}
 
 watch(searchTerm, newTerm => findProducts(newTerm))
 </script>
@@ -12,8 +20,9 @@ watch(searchTerm, newTerm => findProducts(newTerm))
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li>Display suggestions here</li>
+    <ul v-if="products.length" class="list-disc">
+      <li v-for="product in products">{{ product.title }}: {{ product.price }}$</li>
     </ul>
+    <p v-else>No products found...</p>
   </div>
 </template>
